c418431075624c0d86ceae904ee305808cab71c1
chelizichen <1347290221@qq.com>
2023-03-14 18:53:32 +0800

    添加测试代码

--- Test/index.js
+++ Test/index.js
@@ -14,6 +14,14 @@ let server = net.createConnection({
 // #a#1#b#2#c#3#z#
 // [#ENDL#]\n
 
+// [#1]HelloInterFace
+// [#2]TestRet
+// [#3]3000
+// [#4]36
+// [##]
+// #a##a#tom#b#jump#c#12#d#1#z##b##a#测试#z##c#1#z#
+// [#ENDL#]\n
+
 server.write(`
 [#1]HelloInterFace
 [#2]TestRet
