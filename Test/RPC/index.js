719080e6d9435dccd55011f68e558da5d0ed7a9a
chelizichen <1347290221@qq.com>
2023-03-17 18:02:25 +0800

    update:更新结构体

--- Test/RPC/index.js
+++ Test/RPC/index.js
@@ -1,29 +1,27 @@
-// const code = `struct testparams{
-//   helloReq  : {
-//     1 optional message : string;
-//     2 optional id : int;
-//   };
-//   helloRes  : {
-//     1 optional data : string;
-//     2 optional code : int;
-//     3 optional message : string;
-//   };
-//   sayReq  : {
-//     1 optional message : string;
-//     2 optional id : int;
-//   };
-//   sayRes  : {
-//     1 optional data : string;
-//     2 optional code : int;
-//     3 optional message : string;
-//   };
-// };`;
-
 const { readFileSync } = require('fs');
 
-let file = readFileSync("./test.tsu","utf-8").trim().replace("\\n","")
-const map = new Map();
-// let _to_str_ = code.trimStart().trimEnd()
-let reg = new RegExp(/struct(.*)/g,)
-let match = reg.exec(file)
-console.log(match);
+var TarsusStream = function (url) {
+  this._stream = readFileSync(url, "utf-8").trim().replace(/\n|\r/g, "") // 拿到并去除换行
+
+  let body = new RegExp(/struct(.*)};/g,)   // 正则1 拿到 结构体数据
+  let match = body.exec(this._stream)
+
+  let struct = new RegExp(/struct (.*?){ /s)   // 正则2 拿到 结构体名称
+
+  this._struct_name = struct.exec(match)[1] // 没有做trim 
+
+  let struct_match_body = match[1].split(this._struct_name + "{")[1]
+
+  this._data = struct_match_body.split("};").filter(v => v).map(e => e + "}").map(e => e.trim())
+
+  this.readStruct()
+
+}
+
+TarsusStream.prototype.readStruct = function () {
+  this._data.forEach(el => {
+    console.log(el);
+  })
+}
+
+new TarsusStream("./test.tsu")
