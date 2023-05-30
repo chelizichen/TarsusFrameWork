f3d87ce46a654fe0f5fd4042587daee021e7969f
chelizichen <1347290221@qq.com>
2023-05-31 00:33:22 +0800

    optimize: 多进程多实例

--- Test/load_balance/cluster.js
+++ Test/load_balance/cluster.js
@@ -1,43 +1,52 @@
-// 集群策略 开启多端口模式
-
 const cluster = require('cluster');
-const os = require('os');
+const numCPUs = require('os').cpus().length;
+const net = require('net');
 
-const numCPUs = os.cpus().length;
-const ports = [3000, 3001, 3002]; // 端口数组，可以根据需要进行扩展
+const ports = [3000, 4000, 5000]; // 要监听的端口列表
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+  // 主进程
+  console.log(`Master process ID: ${process.pid}`);
 
   // 创建工作进程
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
+  // 监听工作进程的退出事件
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    // 在工作进程退出后重新创建新的工作进程
+    console.log(`Worker process ${worker.process.pid} exited`);
+    console.log(`Starting a new worker...`);
     cluster.fork();
   });
 } else {
-  // 在工作进程中创建服务器监听指定端口
-  const serverConfig = getServerConfig(); // 获取服务端配置
-  const port = ports[cluster.worker.id % ports.length]; // 根据工作进程ID选择端口
+  // 工作进程
+  console.log(`Worker process ID: ${process.pid}`);
 
-  const server = createServer(serverConfig);
-  server.listen(port, () => {
-    console.log(`Worker ${process.pid} is listening on port ${port}`);
+  // 创建 TCP 服务器实例
+  const servers = ports.map(port => {
+    const server = net.createServer();
+
+    // 监听连接事件
+    server.on('connection', socket => {
+      console.log(`Worker process ${process.pid} - Client connected to port ${port}`);
+
+      // 处理接收到的数据
+      socket.on('data', data => {
+        console.log(`Worker process ${process.pid} - Received data from client on port ${port}: ${data}`);
+      });
+
+      // 处理连接断开事件
+      socket.on('end', () => {
+        console.log(`Worker process ${process.pid} - Client disconnected from port ${port}`);
+      });
+    });
+
+    // 启动服务器，开始监听指定端口
+    server.listen(port, () => {
+      console.log(`Worker process ${process.pid} - Server started on port ${port}`);
+    });
+
+    return server;
   });
-}
-
-// 获取服务端配置的示例函数，根据实际需求进行实现
-function getServerConfig() {
-  return {
-    // 返回服务端配置对象
-  };
-}
-
-// 创建服务器的示例函数，根据实际需求进行实现
-function createServer(config) {
-  // 创建服务器逻辑，返回服务器实例
 }
